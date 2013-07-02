1c5f75a0929c987eca64bef35572e272be686eb6
xushiwei <xushiweizh@gmail.com>
2013-07-03 01:53:17 +0800

    todo: rsf, rs.batch, resumable.io.put

--- qiniu/rs.php
+++ qiniu/rs.php
@@ -95,6 +95,7 @@ class Qiniu_RS_EntryPath
 {
 	public $bucket;
 	public $key;
+
 	public function __construct($bucket, $key)
 	{
 		$this->bucket = $bucket;
@@ -109,6 +110,7 @@ class Qiniu_RS_EntryPathPair
 {
 	public $src;
 	public $dest;
+
 	public function __construct($src, $dest)
 	{
 		$this->src = $src;
@@ -118,112 +120,55 @@ class Qiniu_RS_EntryPathPair
 
 // ----------------------------------------------------------
 
+function Qiniu_RS_URIStat($bucket, $key)
+{
+	return '/stat/' . Qiniu_Encode("$bucket:$key");
+}
+
+function Qiniu_RS_URIDelete($bucket, $key)
+{
+	return '/delete/' . Qiniu_Encode("$bucket:$key");
+}
+
+function Qiniu_RS_URICopy($bucketSrc, $keySrc, $bucketDest, $keyDest)
+{
+	return '/copy/' . Qiniu_Encode("$bucketSrc:$keySrc") . '/' . Qiniu_Encode("$bucketDest:$keyDest");
+}
+
+function Qiniu_RS_URIMove($bucketSrc, $keySrc, $bucketDest, $keyDest)
+{
+	return '/move/' . Qiniu_Encode("$bucketSrc:$keySrc") . '/' . Qiniu_Encode("$bucketDest:$keyDest");
+}
+
+// ----------------------------------------------------------
+
 function Qiniu_RS_Stat($self, $bucket, $key) // => ($statRet, $error)
 {
 	global $QINIU_RS_HOST;
-	return Qiniu_Client_Call($self, $QINIU_RS_HOST . Qiniu_RS_URIStat($bucket, $key));
+	$uri = Qiniu_RS_URIStat($bucket, $key);
+	return Qiniu_Client_Call($self, $QINIU_RS_HOST . $uri);
 }
 
 function Qiniu_RS_Delete($self, $bucket, $key) // => $error
 {
 	global $QINIU_RS_HOST;
-	return Qiniu_Client_CallNoRet($self, $QINIU_RS_HOST . Qiniu_RS_URIDelete($bucket, $key));
+	$uri = Qiniu_RS_URIDelete($bucket, $key);
+	return Qiniu_Client_CallNoRet($self, $QINIU_RS_HOST . $uri);
 }
-
 
 function Qiniu_RS_Move($self, $bucketSrc, $keySrc, $bucketDest, $keyDest) // => $error
 {
 	global $QINIU_RS_HOST;
-	return Qiniu_Client_CallNoRet($self, "$QINIU_RS_HOST".Qiniu_RS_URIMove($bucketSrc, $keySrc, $bucketDest, $keyDest));
+	$uri = Qiniu_RS_URIMove($bucketSrc, $keySrc, $bucketDest, $keyDest);
+	return Qiniu_Client_CallNoRet($self, $QINIU_RS_HOST . $uri);
 }
 
 function Qiniu_RS_Copy($self, $bucketSrc, $keySrc, $bucketDest, $keyDest) // => $error
 {
 	global $QINIU_RS_HOST;
-	return Qiniu_Client_CallNoRet($self, "$QINIU_RS_HOST".Qiniu_RS_URICopy($bucketSrc, $keySrc, $bucketDest, $keyDest));
+	$uri = Qiniu_RS_URICopy($bucketSrc, $keySrc, $bucketDest, $keyDest);
+	return Qiniu_Client_CallNoRet($self, $QINIU_RS_HOST . $uri);
 }
-
-
-// ----------------------------------------------------------
-//batch
-
-function Qiniu_RS_Batch($self, $url) // => ($data, $error)
-{
-	global $QINIU_RS_HOST;
-	return Qiniu_Client_CallWithForm($self, $QINIU_RS_HOST . "/batch?", $url);
-}
-
-function Qiniu_RS_BatchStat($self, $entryPaths)
-{
-	$params = '';
-	foreach ($entryPaths as $entryPath) {
-		if ($params === '') {
-			$params = 'op=' . Qiniu_RS_URIStat($entryPath->bucket, $entryPath->key);
-			continue;
-		}
-		$params .= '&op=' . Qiniu_RS_URIStat($entryPath->bucket, $entryPath->key);
-	}
-	return Qiniu_RS_Batch($self,$params);
-}
-
-function Qiniu_RS_BatchDelete($self, $entryPaths)
-{
-	$params = '';
-	foreach ($entryPaths as $entryPath) {
-		if ($params == '') {
-			$params = 'op=' . Qiniu_RS_URIDelete($entryPath->bucket, $entryPath->key);
-			continue;
-		}
-		$params .= '&op=' . Qiniu_RS_URIDelete($entryPath->bucket, $entryPath->key);
-	}
-	return Qiniu_RS_Batch($self, $params);
-}
-
-function Qiniu_RS_BatchMove($self, $entryPairs)
-{
-	$params = '';
-	foreach ($entryPairs as $entryPair) {
-		if ($params == '') {
-			$params = 'op=' . Qiniu_RS_URIMove($entryPair->src->bucket, $entryPair->src->key, $entryPair->dest->bucket, $entryPair->dest->key);
-			continue;
-		}
-		$params .= '&op=' . Qiniu_RS_URIMove($entryPair->src->bucket, $entryPair->src->key, $entryPair->dest->bucket, $entryPair->dest->key);
-	}
-	return Qiniu_RS_Batch($self, $params);
-}
-
-function Qiniu_RS_BatchCopy($self, $entryPairs)
-{
-	$params = '';
-	foreach ($entryPairs as $entryPair) {
-		if ($params == '') {
-			$params = 'op=' . Qiniu_RS_URICopy($entryPair->src->bucket, $entryPair->src->key, $entryPair->dest->bucket, $entryPair->dest->key);
-			continue;
-		}
-		$params .= '&op=' . Qiniu_RS_URICopy($entryPair->src->bucket, $entryPair->src->key, $entryPair->dest->bucket, $entryPair->dest->key);
-	}
-	return Qiniu_RS_Batch($self, $params);
-}
-
 
 // ----------------------------------------------------------
 
-function Qiniu_RS_URIStat($bucket, $key) //	=> $entryURIEncoded
-{
-	return "/stat/" . Qiniu_Encode("$bucket:$key");
-}
-
-function Qiniu_RS_URIDelete($bucket, $key)
-{
-	return "/delete/" . Qiniu_Encode("$bucket:$key");
-}
-
-function Qiniu_RS_URICopy($bucketSrc, $keySrc, $bucketDest, $keyDest)
-{
-	return "/copy/" . Qiniu_Encode("$bucketSrc:$keySrc") . "/" . Qiniu_Encode("$bucketDest:$keyDest");
-}
-
-function Qiniu_RS_URIMove($bucketSrc, $keySrc, $bucketDest, $keyDest)
-{
-	return "/move/" . Qiniu_Encode("$bucketSrc:$keySrc") . "/" . Qiniu_Encode("$bucketDest:$keyDest");
-}
--- tests/IoTest.php
+++ tests/IoTest.php
@@ -9,18 +9,20 @@ class IoTest extends PHPUnit_Framework_TestCase
 
 	public function setUp()
 	{
-		$this->client = new Qiniu_Client(null);
+		$this->client = new Qiniu_MacHttpClient(null);
 		$this->bucket = getenv("QINIU_BUCKET_NAME");
 	}
 
 	public function testPutFile()
 	{
-		$key = rand();
+		$key = 'testPutFile';
+		$err = Qiniu_RS_Delete($this->client, $this->bucket, $key);
+
 		$putPolicy = new Qiniu_RS_PutPolicy($this->bucket);
 		$upToken = $putPolicy->Token(null);
 		$putExtra = new Qiniu_PutExtra();
 		$putExtra->CheckCrc = 1;
-		list($ret, $err) = Qiniu_PutFile($upToken, $key, __File__, $putExtra);
+		list($ret, $err) = Qiniu_PutFile($upToken, $key, __file__, $putExtra);
 		$this->assertArrayHasKey('hash', $ret);
 		$this->assertNull($err);
 
@@ -30,11 +32,12 @@ class IoTest extends PHPUnit_Framework_TestCase
 
 	public function testPut()
 	{
-		$key = rand();
+		$key = 'testPut';
+		$err = Qiniu_RS_Delete($this->client, $this->bucket, $key);
+
 		$putPolicy = new Qiniu_RS_PutPolicy($this->bucket);
 		$upToken = $putPolicy->Token(null);
-		$putExtra = new Qiniu_PutExtra();
-		list($ret, $err) = Qiniu_Put($upToken, $key, "hello world!", $putExtra);
+		list($ret, $err) = Qiniu_Put($upToken, $key, "hello world!", null);
 		$this->assertArrayHasKey('hash', $ret);
 		$this->assertNull($err);
 
