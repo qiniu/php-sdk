00a85200a75606ac17172d83c92b8d08c63f6d9b
dtynn <dtynn@163.com>
2013-10-25 14:23:07 +0800

    putPolicy

--- tests/IoTest.php
+++ tests/IoTest.php
@@ -67,7 +67,56 @@ class IoTest extends PHPUnit_Framework_TestCase
 		list($ret, $err) = Qiniu_Put($upToken, $key, "hello world!", null);
 		$this->assertNull($ret);
 		$this->assertEquals($err->Err, 'exceed FsizeLimit');
+		var_dump($err);
+	}
+
+	public function testPut_mime_save()
+	{
+		$key = 'testPut_mime_save' . getTid();
+		$err = Qiniu_RS_Delete($this->client, $this->bucket, $key);
+
+		$putPolicy = new Qiniu_RS_PutPolicy($this->bucket);
+		$putPolicy->DetectMime = 1;
+		$putPolicy->SaveKey = $key;
+		$upToken = $putPolicy->Token(null);
+		$putExtra = new Qiniu_PutExtra();
+		$putExtra->MimeType = 'image/jpg';
+		list($ret, $err) = Qiniu_PutFile($upToken, null, __file__, $putExtra);
+		$this->assertNull($err);
+
+		list($ret, $err) = Qiniu_RS_Stat($this->client, $this->bucket, $key);
+		$this->assertNull($err);
+		$this->assertEquals($ret['mimeType'], 'application/x-httpd-php');
 		var_dump($ret);
+
+		$err = Qiniu_RS_Delete($this->client, $this->bucket, $key);
+		$this->assertNull($err);
+	}
+
+	public function testPut_exclusive()
+	{
+		$key = 'testPut_exclusive' . getTid();
+		$scope = $this->bucket . ':' . $key;
+		$err = Qiniu_RS_Delete($this->client, $this->bucket, $key);
+
+		$putPolicy = new Qiniu_RS_PutPolicy($scope);
+		$putPolicy->InsertOnly = 1;
+		$upToken = $putPolicy->Token(null);
+
+		list($ret, $err) = Qiniu_Put($upToken, $key, "hello world!", null);
+		$this->assertNull($err);
+		list($ret, $err) = Qiniu_PutFile($upToken, $key, __file__, null);
+		$this->assertNull($ret);
+		$this->assertEquals($err->Err, 'file exists');
+		var_dump($err);
+
+		list($ret, $err) = Qiniu_RS_Stat($this->client, $this->bucket, $key);
+		$this->assertNull($err);
+		$this->assertEquals($ret['mimeType'], 'application/octet-stream');
+		var_dump($ret);
+
+		$err = Qiniu_RS_Delete($this->client, $this->bucket, $key);
+		$this->assertNull($err);
 	}
 }
 
