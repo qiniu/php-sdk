10b20eef85636851f89998dfce800c7e3fba0523
rwifeng <rw.ifeng@gmail.com>
2013-12-28 00:43:50 +0800

    add conf

--- qiniu/conf.php
+++ qiniu/conf.php
@@ -3,13 +3,14 @@
 global $QINIU_UP_HOST;
 global $QINIU_RS_HOST;
 global $QINIU_RSF_HOST;
- 
+
 global $QINIU_ACCESS_KEY;
 global $QINIU_SECRET_KEY;
 
 $QINIU_UP_HOST	= 'http://up.qiniu.com';
 $QINIU_RS_HOST	= 'http://rs.qbox.me';
 $QINIU_RSF_HOST	= 'http://rsf.qbox.me';
+$QINIU_CDN_REFRESH_HOST = 'http://cdnmgr.qbox.me:15001';
 
 $QINIU_ACCESS_KEY	= '<Please apply your access key>';
 $QINIU_SECRET_KEY	= '<Dont send your secret key to anyone>';
