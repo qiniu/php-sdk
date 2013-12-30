b554044627b868870ca71c6777f2874607551801
rwifeng <rw.ifeng@gmail.com>
2013-12-30 13:57:28 +0800

    fix path

--- qiniu/cdn.php
+++ qiniu/cdn.php
@@ -1,14 +1,15 @@
 <?php
-
 require_once("http.php");
 
-
-function Qiniu_CDN_Refresh($self, $urls, $dirs)
+function Qiniu_CDN_Refresh($self, $urls, $dirs = array())
 {
     global $QINIU_CDN_REFRESH_HOST;
-    $url = $QINIU_CDN_REFRESH_HOST . '/cdn/refresh';
+    $url = $QINIU_CDN_REFRESH_HOST . '/refresh/';
     $params = 'urls=' . implode('&urls=', $urls);
-    $params .= '&dirs=' . implode('&dirs=', $dirs);
+
+    if (!empty($dirs)) {
+        $params .= '&dirs=' . implode('&dirs=', $dirs);
+    }
 
     return Qiniu_Client_CallWithForm($self, $url, $params);
 }
