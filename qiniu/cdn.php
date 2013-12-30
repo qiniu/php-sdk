6a219bd67ac62143f3f858c7c8e8b5fe74e52f39
rwifeng <rw.ifeng@gmail.com>
2013-12-30 10:04:08 +0800

    fix

--- qiniu/cdn.php
+++ qiniu/cdn.php
@@ -8,7 +8,7 @@ function Qiniu_CDN_Refresh($self, $urls, $dirs)
     global $QINIU_CDN_REFRESH_HOST;
     $url = $QINIU_CDN_REFRESH_HOST . '/cdn/refresh';
     $params = 'urls=' . implode('&urls=', $urls);
-    $params .= '&dirs=' . implode('&dirs=', $dirs)
+    $params .= '&dirs=' . implode('&dirs=', $dirs);
 
     return Qiniu_Client_CallWithForm($self, $url, $params);
 }
