10164c8a15ab8212d6e9eb58821d79d066044fb0
rwifeng <rw.ifeng@gmail.com>
2013-12-30 15:21:20 +0800

    urlencode

--- qiniu/cdn.php
+++ qiniu/cdn.php
@@ -5,11 +5,20 @@ function Qiniu_CDN_Refresh($self, $urls, $dirs = array())
 {
     global $QINIU_CDN_REFRESH_HOST;
     $url = $QINIU_CDN_REFRESH_HOST . '/refresh/';
-    $params = 'urls=' . implode('&urls=', $urls);
+
+    $params = 'urls=' . Qiniu_Http_Build_Query($urls, '&urls=');
 
     if (!empty($dirs)) {
-        $params .= '&dirs=' . implode('&dirs=', $dirs);
+        $params .= '&dirs=' . Qiniu_Http_Build_Query($dirs, '&urls=');
     }
 
     return Qiniu_Client_CallWithForm($self, $url, $params);
+}
+
+function Qiniu_Http_Build_Query($query, $sep)
+{
+    array_walk($query, function(&$val, $key){
+        $val = urlencode($val);
+    });
+    return implode($query, $sep);
 }
