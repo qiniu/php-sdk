99a2ed89a49088c49ea89f8cd334ddf21acb78a0
Bai Long <slongbai@gmail.com>
2015-04-18 16:18:19 +0800

    update version [ci skip]

--- src/Qiniu/Config.php
+++ src/Qiniu/Config.php
@@ -3,7 +3,7 @@ namespace Qiniu;
 
 final class Config
 {
-    const SDK_VER = '7.0.0';
+    const SDK_VER = '7.0.2';
 
     const IO_HOST  = 'http://iovip.qbox.me';            // 七牛源站Host
     const RS_HOST  = 'http://rs.qbox.me';               // 文件元信息管理操作Host
