e7e17090849d61bf4b93383482b008a30fae7bc4
longbai <slongbai@gmail.com>
2015-07-23 22:42:37 +0800

    add test

--- tests/Qiniu/Tests/ResumeUpTest.php
+++ tests/Qiniu/Tests/ResumeUpTest.php
@@ -29,6 +29,19 @@ class ResumeUpTest extends \PHPUnit_Framework_TestCase
         unlink($tempFile);
     }
 
+    public function test4ML2()
+    {
+        $key = 'resumePutFile4ML';
+        $zone = new Zone('http://up.fake.com', 'http://up.qiniu.com');
+        $cfg = new Config($zone);
+        $upManager = new UploadManager($cfg);
+        $token = $this->auth->uploadToken($this->bucketName, $key);
+        $tempFile = qiniuTempFile(4*1024*1024+10);
+        list($ret, $error) = $upManager->putFile($token, $key, $tempFile);
+        $this->assertNull($error);
+        $this->assertNotNull($ret['hash']);
+        unlink($tempFile);
+    }
     // public function test8M()
     // {
     //     $key = 'resumePutFile8M';
