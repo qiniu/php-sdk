2601b64ca126732941fc405de2c2c5f1e94038f4
rwifeng <rw.ifeng@gmail.com>
2015-07-29 20:17:46 +0800

    add callback file

---
+++ demos/callback.php
@@ -0,0 +1,31 @@
+<?php
+require_once '../autoload.php';
+
+use Qiniu\Auth;
+
+
+$accessKey = 'QWYn5TFQsLLU1pL5MFEmX3s5DmHdUThav9WyOWOm';
+$secretKey = 'Bxckh6FA-Fbs9Yt3i3cbKVK22UPBmAOHJcL95pGz';
+$auth = new Auth($accessKey, $secretKey);
+
+//获取回调的body信息
+$callbackBody = file_get_contents('php://input');
+
+//回调的contentType
+$contentType = 'application/x-www-form-urlencoded';
+
+//回调的签名信息，可以验证该回调是否来自七牛
+$authorization = $_SERVER['HTTP_AUTHORIZATION'];
+
+//七牛回调的url，具体可以参考：http://developer.qiniu.com/docs/v6/api/reference/security/put-policy.html
+$url = 'http://172.30.251.210/callback.php';
+
+$isQiniuCallback = $auth->verifyCallback($contentType, $authorization, $url, $callbackBody);
+
+if ($isQiniuCallback) {
+	$resp = array('ret' => 'success');
+} else {
+	$resp = array('ret' => 'failed');
+}
+
+echo json_encode($resp);
