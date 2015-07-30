e24abb39bca0b8fa1f4c514d924fa005b3e59ab8
rwifeng <rw.ifeng@gmail.com>
2015-07-30 10:33:45 +0800

    typo fix

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 
-$path = __DIR__ . DIRECTORY_SEPARATOR . 'src'; 
+$path = __DIR__ . DIRECTORY_SEPARATOR . 'src';
 set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 
 spl_autoload_extensions('.php');
