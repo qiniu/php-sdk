0958e6392d736e712bd84026784b6ca26d1e19c8
zgldh <zgldh@hotmail.com>
2015-09-26 17:27:18 +0800

    照顾老用户。notify url是选填的

--- src/QiniuFilesystemServiceProvider.php
+++ src/QiniuFilesystemServiceProvider.php
@@ -33,7 +33,7 @@ class QiniuFilesystemServiceProvider extends ServiceProvider
                     $config['secret_key'],
                     $config['bucket'],
                     $domains,
-                    $config['notify_url']
+                    $config['notify_url']?$config['notify_url']:null
                 );
                 $file_system = new Filesystem($qiniu_adapter);
                 $file_system->addPlugin(new PrivateDownloadUrl());
