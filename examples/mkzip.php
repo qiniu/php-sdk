b2fdb4b64c7931cb83c708ecff2f8cc5b4675fa9
rwifeng <rw.ifeng@gmail.com>
2016-01-11 21:15:58 +0800

    split examples

--- examples/mkzip.php
+++ examples/mkzip.php
@@ -4,12 +4,13 @@ require_once __DIR__ . '/../autoload.php';
 use Qiniu\Auth;
 use Qiniu\Processing\PersistentFop;
 
-$accessKey = 'Access_Key';
-$secretKey = 'Secret_Key';
+// 去我们的portal 后台来获取AK, SK
+$accessKey = '4brtJLyWA3bplJKlkn7ZypPbzKcS-58MsF1cnsF4';
+$secretKey = 'jt8qbHTrBFAl6HZNt9Mmd2pcx122aJlJ-5mgS-7g';
 $auth = new Auth($accessKey, $secretKey);
 
 
-$bucket = 'Bucket_Name';
+$bucket = 'rwxf';
 $key = '1.png';
 
 // 异步任务的队列， 去后台新建： https://portal.qiniu.com/mps/pipeline
@@ -18,8 +19,8 @@ $pipeline = 'abc';
 $pfop = new PersistentFop($auth, $bucket, $pipeline);
 
 // 进行zip压缩的url
-$url1 = 'http://Bucket_Name.qiniudn.com/php-logo.png';
-$url2 = 'http://Bucket_Name.qiniudn.com/php-sdk.html';
+$url1 = 'http://rwxf.qiniudn.com/php-logo.png';
+$url2 = 'http://rwxf.qiniudn.com/1.png';
 
 //压缩后的key
 $zipKey = 'test.zip';
