14d7cb4448a3d01cd9b1941337a423f80906ccb5
huxicong <huxicong@qiniu.com>
2016-03-19 17:48:36 +0800

    addfname

--- src/Qiniu/Storage/UploadManager.php
+++ src/Qiniu/Storage/UploadManager.php
@@ -46,7 +46,8 @@ final class UploadManager
         $data,
         $params = null,
         $mime = 'application/octet-stream',
-        $checkCrc = false
+        $checkCrc = false,
+        $filePath=null
     ) {
         $params = self::trimParams($params);
         return FormUploader::put(
@@ -56,7 +57,8 @@ final class UploadManager
             $this->config,
             $params,
             $mime,
-            $checkCrc
+            $checkCrc,
+            $filePath
         );
     }
 
