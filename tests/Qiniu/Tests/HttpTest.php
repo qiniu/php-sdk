2a9e169c29333fbdbe2e66b98b51a52a341abab7
rwifeng <rw.ifeng@gmail.com>
2016-07-11 15:59:53 +0800

    modify test case

--- tests/Qiniu/Tests/HttpTest.php
+++ tests/Qiniu/Tests/HttpTest.php
@@ -26,9 +26,9 @@ class HttpTest extends \PHPUnit_Framework_TestCase
     public function testPost()
     {
         $response = Client::post('baidu.com', null);
-        $this->assertEquals($response->statusCode, 405);
+        $this->assertEquals($response->statusCode, 200);
         $this->assertNotNull($response->body);
-        $this->assertNotNull($response->error);
+        $this->assertNull($response->error);
     }
 
     public function testPostQiniu()
