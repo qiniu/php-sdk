e931002c34c0f368b156fd52c6f15aab64575313
rwifeng <rw.ifeng@gmail.com>
2016-08-09 14:32:36 +0800

    add multi zone  case

--- tests/Qiniu/Tests/ZoneTest.php
+++ tests/Qiniu/Tests/ZoneTest.php
@@ -51,6 +51,30 @@ class ZoneTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('https://up-z1.qbox.me', $upHosts[0]);
     }
 
+    public function testUpHostByToken()
+    {
+        $uptoken_bc = 'QWYn5TFQsLLU1pL5MFEmX3s5DmHdUThav9WyOWOm:bl77a3xPdTyBNYFGVRy
+        oIQNyp_s=:eyJzY29wZSI6InBocHNkay1iYyIsImRlYWRsaW5lIjoxNDcwNzI1MzE1LCJ1cEhvc
+        3RzIjpbImh0dHA6XC9cL3VwLXoxLnFpbml1LmNvbSIsImh0dHA6XC9cL3VwbG9hZC16MS5xaW5p
+        dS5jb20iLCItSCB1cC16MS5xaW5pdS5jb20gaHR0cDpcL1wvMTA2LjM4LjIyNy4yNyJdfQ==';
+
+        $upHost = $this->zone->getUpHostByToken($uptoken_bc);
+        $this->assertEquals('http://up-z1.qiniu.com', $upHost);
+
+        $upHostBackup = $this->zone->getBackupUpHostByToken($uptoken_bc);
+        $this->assertEquals('http://upload-z1.qiniu.com', $upHostBackup);
+
+
+        $uptoken_bc_https = 'QWYn5TFQsLLU1pL5MFEmX3s5DmHdUThav9WyOWOm:7I47O-vFcN5TKO
+        6D7cobHPVkyIA=:eyJzY29wZSI6InBocHNkay1iYyIsImRlYWRsaW5lIjoxNDcwNzIyNzQ1LCJ1c
+        Ehvc3RzIjpbImh0dHBzOlwvXC91cC16MS5xYm94Lm1lIl19';
+        $upHost = $this->zoneHttps->getUpHostByToken($uptoken_bc_https);
+        $this->assertEquals('https://up-z1.qbox.me', $upHost);
+
+        $upHostBackup = $this->zoneHttps->getBackupUpHostByToken($uptoken_bc_https);
+        $this->assertEquals('https://up-z1.qbox.me', $upHostBackup);
+    }
+
     public function testIoHosts()
     {
 
