c4426110cab2ab154d9c6e2d141780a611818ff3
rwifeng <rw.ifeng@gmail.com>
2016-09-12 19:39:57 +0800

    rm ak sk

--- examples/fresh.php
+++ examples/fresh.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/../autoload.php';
 use Qiniu\Auth;
 use Qiniu\Cdn\CacheManager;
 
-$accessKey = '4brtJLyWA3bplJKlkn7ZypPbzKcS-58MsF1cnsF4';
-$secretKey = 'jt8qbHTrBFAl6HZNt9Mmd2pcx122aJlJ-5mgS-7g';
+$accessKey = '';
+$secretKey = '';
 
 //初始化Auth状态：
 $auth = new Auth($accessKey, $secretKey);
