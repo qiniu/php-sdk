be1af9b40d86a0b9140ffe30e54c1eca46b1d759
rwifeng <rw.ifeng@gmail.com>
2017-06-21 10:27:11 +0800

    modify examples

--- examples/cdn_manager_timestamp_antileech.php
+++ examples/cdn_manager_timestamp_antileech.php
@@ -6,15 +6,14 @@ use \Qiniu\Cdn\CdnManager;
 
 //创建时间戳防盗链
 //时间戳防盗链密钥，后台获取
-$encryptKey = '9c897c897d997f99fda9069a988bd6ab2fb9e8b9';
+$encryptKey = 'your_domain_timestamp_antileech_encryptkey';
 
 //带访问协议的域名
-$url1 = 'http://sf.szcf.duochang.cc/TS/00027288.ts?avinfo';
-$url2 = 'http://sf.szcf.duochang.cc/TS/00027288.ts';
+$url1 = 'http://phpsdk.qiniuts.com/24.jpg?avinfo';
+$url2 = 'http://phpsdk.qiniuts.com/24.jpg';
 
 //有效期时间（单位秒）
 $durationInSeconds = 3600;
 
 $signedUrl = CdnManager::createTimestampAntiLeechUrl($url1, $encryptKey, $durationInSeconds);
 print($signedUrl);
-print("\n");
