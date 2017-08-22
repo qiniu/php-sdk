069af0ce04dab72bbd9ef3708838e65832994e12
anka <425732469@qq.com>
2017-08-22 16:37:00 +0800

    修改

--- examples/saveas.php
+++ examples/saveas.php
@@ -1,28 +1,28 @@
 <?php
-    require_once __DIR__ . '/../autoload.php';
-    
-    use Qiniu\Auth;
-    use Qiniu\Processing\PersistentFop;
+require_once __DIR__ . '/../autoload.php';
 
-    // 后台来获取AK, SK
-    $accessKey = 'Access_Key';
-    $secretKey = 'Secret_Key';
+use Qiniu\Auth;
+use Qiniu\Processing\PersistentFop;
 
-    //生成EncodedEntryURI的值
-    $entry='<bucket>:<Key>';//<Key>为生成缩略图的文件名
-    //生成的值
-    $encodedEntryURI=\Qiniu\base64_urlSafeEncode($entry);
+// 后台来获取AK, SK
+$accessKey = 'Access_Key';
+$secretKey = 'Secret_Key';
 
-    //使用SecretKey对新的下载URL进行HMAC1-SHA1签名
-    $newurl = "78re52.com1.z0.glb.clouddn.com/resource/Ship.jpg?imageView2/2/w/200/h/200|saveas/".$encodedEntryURI;
+//生成EncodedEntryURI的值
+$entry='<bucket>:<Key>';//<Key>为生成缩略图的文件名
+//生成的值
+$encodedEntryURI=\Qiniu\base64_urlSafeEncode($entry);
 
-    $sign = hash_hmac("sha1", $newurl, $secretKey, true);
+//使用SecretKey对新的下载URL进行HMAC1-SHA1签名
+$newurl = "78re52.com1.z0.glb.clouddn.com/resource/Ship.jpg?imageView2/2/w/200/h/200|saveas/".$encodedEntryURI;
 
-    //对签名进行URL安全的Base64编码
-    $encodedSign = \Qiniu\base64_urlSafeEncode($sign);
-    //最终得到的完整下载URL
-    $finalURL = "http://".$newurl."/sign/".$accessKey.":".$encodedSign;
+$sign = hash_hmac("sha1", $newurl, $secretKey, true);
 
-    $callbackBody = file_get_contents("$finalURL");
+//对签名进行URL安全的Base64编码
+$encodedSign = \Qiniu\base64_urlSafeEncode($sign);
+//最终得到的完整下载URL
+$finalURL = "http://".$newurl."/sign/".$accessKey.":".$encodedSign;
 
-    echo $callbackBody;
+$callbackBody = file_get_contents("$finalURL");
+
+echo $callbackBody;
