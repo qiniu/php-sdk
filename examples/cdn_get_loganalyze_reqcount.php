7a5770cef4eed1bb86741ac3e2a90a3cb038ade2
rexshi <rexshi@pgyer.com>
2018-08-15 14:18:19 +0800

    Add cdn loganalyze API

---
+++ examples/cdn_get_loganalyze_reqcount.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once __DIR__ . '/../autoload.php';
+
+use \Qiniu\Cdn\CdnManager;
+
+$accessKey = getenv('QINIU_ACCESS_KEY');
+$secretKey = getenv('QINIU_SECRET_KEY');
+
+$auth = new Qiniu\Auth($accessKey, $secretKey);
+$cdnManager = new CdnManager($auth);
+
+$domains = array(
+    "javasdk.qiniudn.com",
+    "phpsdk.qiniudn.com"
+);
+
+list($logListData, $getLogErr) = $cdnManager->getCdnLoganalyzeReqcount($domains, '1hour', 'global', '2018-08-01', '2018-08-03');
+if ($getLogErr != null) {
+    var_dump($getLogErr);
+} else {
+    echo "get cdn log analyze top count url success\n";
+    print_r($logListData);
+}
