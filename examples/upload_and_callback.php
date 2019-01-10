3a4529d4baf7084714b4dafa6ed2ba364654848c
anka <425732469@qq.com>
2019-01-10 11:04:16 +0800

    update

--- examples/upload_and_callback.php
+++ examples/upload_and_callback.php
@@ -12,7 +12,7 @@ use Qiniu\Storage\UploadManager;
 // $config = new Config($zone);
 
 $accessKey = 'gwd_gV4gPKZZsmEOvAuNU1AcumicmuHooTfu64q5';
-$secretKey = '9G4isTkVuj5ITPqH1ajhljJMTc2k4m-hZh5r5ZsK';
+$secretKey = '';
 $bucket = 'file';
 $auth = new Auth($accessKey, $secretKey);
 
