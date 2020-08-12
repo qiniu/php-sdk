8e20033144adcdb2698aff59fc794ce3c2c2743e
timhbw <timhbw@gmail.com>
2020-08-13 00:00:19 +0800

    update censor_video.php

--- examples/censor_video.php
+++ examples/censor_video.php
@@ -3,23 +3,25 @@
 require_once __DIR__ . '/../autoload.php';
 
 use Qiniu\Auth;
-use Qiniu\Http\Client;
+use Qiniu\Config;
+use Qiniu\Storage\ArgusManager;
 
 // 控制台获取密钥：https://portal.qiniu.com/user/key
 $accessKey = getenv('QINIU_ACCESS_KEY');
 $secretKey = getenv('QINIU_SECRET_KEY');
 
+
 $auth = new Auth($accessKey, $secretKey);
 
-$config = new \Qiniu\Config();
-$argusManager = new \Qiniu\Storage\ArgusManager($auth, $config);
+$config = new Config();
+$argusManager = new ArgusManager($auth, $config);
 
 // 视频内容审核
 // 参考文档：https://developer.qiniu.com/censor/api/5620/video-censor
 
 $body = '{
     "data":{
-        "uri":"https://xxxx.com/test0527.mp4"
+        "uri":"https://dn-mars-assets.qbox.me/Fi1UC6waXtXYCpnTGHa8XxIziGNk"
     },
     "params":{
         "scenes":[
@@ -31,7 +33,7 @@ $body = '{
     }
 }';
 
-list($jobid, $err) = $argusManager->pulpVideo($body);
+list($jobid, $err) = $argusManager->censorVideo($body);
 
 if ($err !== null) {
     var_dump($err);
@@ -39,7 +41,7 @@ if ($err !== null) {
     echo "job_id is: $jobid\n";
 }
 
-// 查询内容审核任务的进度和状态
+// 查询视频审核结果
 list($ret, $err) = $argusManager->censorStatus($jobid);
 echo "\n====> job status: \n";
 
