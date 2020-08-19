3e4a8691cd7bf785d7c91fb5979f47293961ea18
timhbw <timhbw@gmail.com>
2020-08-19 16:44:53 +0800

    test:modify HttpTest.php

--- tests/Qiniu/Tests/HttpTest.php
+++ tests/Qiniu/Tests/HttpTest.php
@@ -23,6 +23,24 @@ class HttpTest extends \PHPUnit_Framework_TestCase
         $this->assertNotNull($response->error);
     }
 
+    public function testDelete()
+    {
+        $response = Client::delete('uc.qbox.me/bucketTagging', array());
+        $this->assertEquals($response->statusCode, 401);
+        $this->assertNotNull($response->body);
+        $this->assertNotNull($response->error);
+    }
+
+    public function testDeleteQiniu()
+    {
+        $response = Client::delete('uc.qbox.me/bucketTagging', array());
+        $this->assertEquals(401, $response->statusCode);
+        $this->assertNotNull($response->body);
+        $this->assertNotNull($response->xReqId());
+        $this->assertNotNull($response->xLog());
+        $this->assertNotNull($response->error);
+    }
+
     public function testPost()
     {
         $response = Client::post('qiniu.com', null);
@@ -35,6 +53,24 @@ class HttpTest extends \PHPUnit_Framework_TestCase
     {
         $response = Client::post('upload.qiniu.com', null);
         $this->assertEquals($response->statusCode, 400);
+        $this->assertNotNull($response->body);
+        $this->assertNotNull($response->xReqId());
+        $this->assertNotNull($response->xLog());
+        $this->assertNotNull($response->error);
+    }
+
+    public function testPut()
+    {
+        $response = Client::PUT('uc.qbox.me/bucketTagging', null);
+        $this->assertEquals($response->statusCode, 401);
+        $this->assertNotNull($response->body);
+        $this->assertNotNull($response->error);
+    }
+
+    public function testPutQiniu()
+    {
+        $response = Client::put('uc.qbox.me/bucketTagging', null);
+        $this->assertEquals(401, $response->statusCode);
         $this->assertNotNull($response->body);
         $this->assertNotNull($response->xReqId());
         $this->assertNotNull($response->xLog());
